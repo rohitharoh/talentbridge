b809319c537aefa71e2320caa6a20388ae01b390
rohitharoh <rohitha.k95@gmail.com>
2018-10-09 18:21:07 +0530

    added contacts page and modified about page

---
+++ genix/js/contacts.js
@@ -0,0 +1,24 @@
+
+	 function initMap() {
+             var uluru = {lat: 12.881899, lng:  77.595666};
+             var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+             var marker = new google.maps.Marker({position: uluru, map: map});
+             }
+     function validate(myform) {
+	        var name=document.myform.name.value;  
+	        var mail=document.myform.email_address.value;  
+	        var message=document.myform.message.value;  
+	        if (name==null || name==""){  
+                alert("All feilds are mandatory");  
+                return false;   
+                }
+            else if(mail==null || mail==""){
+		        alert("All feilds are mandatory");  
+                return false;  
+                }
+            else if(message==null || message==""){
+		        alert("All feilds are mandatory");  
+                return false;  
+                }
+        }
+	
